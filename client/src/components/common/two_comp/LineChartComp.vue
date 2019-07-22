595a5371e803b55a532ec84081b4fa482eef4a20
leejj <leejj@wemb.co.kr>
2019-07-22 18:14:21 +0900

    - echart 추가 - Line/Pie 차트 및 샘플 추가

---
+++ client/src/components/common/two_comp/LineChartComp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div :style="props.style">
+        <v-chart :options="props.option"/>
+    </div>
+</template>
+
+<script>
+
+import ECharts from 'vue-echarts' // refers to components/ECharts.vue in webpack
+
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/polar'
+
+export default {
+    type:'two_comp',
+    name:'line-chart-comp',
+    props: ['props'],
+    data () {
+        return {}
+    },
+    components : {
+        'v-chart': ECharts
+    },
+    created() {
+        console.log('created')
+    },
+    mounted() {
+        console.log('mounted');
+    },
+    destroyed() {
+        console.log('destroyed')
+    }
+}
+</script>
+
+<style>
+    .echarts {
+        width: 100%;
+        height: 100%;
+    }
+</style>
